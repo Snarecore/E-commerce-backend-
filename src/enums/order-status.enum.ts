517387fc0dcc66f4707e1a2b7250ea619cf14b15
kazi-ziaur-rahman-majba <majba9399@gmail.com>
2026-08-22 19:54:59 +0600

    fix(order): add Processing and Shipped to OrderStatus enum

--- src/enums/order-status.enum.ts
+++ src/enums/order-status.enum.ts
@@ -1,6 +1,8 @@
 export enum OrderStatus {
     ORDER_PLACED = 'Order Placed',
+    PROCESSING = 'Processing',
     PREPARING_ORDER = 'Preparing Order',
+    SHIPPED = 'Shipped',
     LOADED_FOR_DELIVERY = 'Loaded for Delivery',
     HANDED_OVER_TO_COURIER = 'Handed Over to Courier',
     OUT_FOR_DELIVERY = 'Out for Delivery',
